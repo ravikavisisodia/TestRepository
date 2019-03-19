766a7b384ae2bda813013cefd450073754da81b3
Ravi Sisodia <ravikavisisodia@gmail.com>
2019-03-19 17:10:16 +0530

    Changes

---
+++ TestRepository/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  TestRepository
+//
+//  Created by Ravi Sisodia on 19/03/19.
+//  Copyright © 2019 CultureAlley. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+	@IBAction func userDidClickSettingsBarButtonItem(_ sender: UIBarButtonItem) {
+		// TODO: open settings
+	}
+}
