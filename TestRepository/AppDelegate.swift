2d4010f795a21d58f25e3cb31f0fbf44c9782a62
Ravi Sisodia <ravikavisisodia@gmail.com>
2019-03-19 18:22:49 +0530

    Changes

--- TestRepository/AppDelegate.swift
+++ TestRepository/AppDelegate.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	var window: UIWindow?
+	var isAuthenticatedDeviceOwner = false
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
@@ -21,19 +23,36 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	func applicationWillResignActive(_ application: UIApplication) {
 		// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
 		// Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+		print("applicationWillResignActive")
 	}
 
 	func applicationDidEnterBackground(_ application: UIApplication) {
 		// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
 		// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+		self.isAuthenticatedDeviceOwner = false
+		print("applicationDidEnterBackground")
 	}
 
 	func applicationWillEnterForeground(_ application: UIApplication) {
 		// Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+		print("applicationWillEnterForeground")
 	}
 
 	func applicationDidBecomeActive(_ application: UIApplication) {
 		// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+		print("applicationDidBecomeActive")
+		guard !self.isAuthenticatedDeviceOwner else {
+			return
+		}
+		guard LAContext().canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: NSErrorPointer(nilLiteral: ())) else {
+			return
+		}
+		LAContext().evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "To Log into the app") { (flag, error) in
+			guard flag else {
+				return print("DeviceOwnerAuthIssue: \(error?.localizedDescription ?? "NA")")
+			}
+			self.isAuthenticatedDeviceOwner = true
+		}
 	}
 
 	func applicationWillTerminate(_ application: UIApplication) {
